4bdb755cc3cd196cd428e040b4cce3bf8390ffa2
MehdiAgazade <agazademehdi371@gmail.com>
2025-11-11 21:20:33 +0400

    modified

---
+++ main.py
@@ -0,0 +1,51 @@
+# Task - 1
+
+# my_list = [1, 2, 3, 4, 5]
+# print(my_list)
+
+# Task - 2
+
+# my_list = ["salam", "necesen", "ne var", "ne yox"]
+# print(my_list[0])
+
+# Task - 3
+
+# my_list = [1, 21.23, "salam", True, "necesen"]
+# print(my_list[4])
+
+# Task - 4
+
+# my_list = [1, 23, 43, 65, 45]
+# my_list[1] = 100
+# print(my_list)
+
+# Task - 5
+
+# my_list = ["salam", "bir", "iki", "uc", "necesen", "hello"]
+# my_list.append("men")
+# print(my_list)
+
+# Task - 6
+
+# my_list = ["salam", "bir", "iki", "uc", "necesen", "hello", "men"]
+# my_list.pop()
+# print(my_list)
+
+# Task - 7
+
+# Basaaa dusmeddimmmm helppppppppppppppppppp
+
+# Task - 8
+
+# my_list = ["salam", "bir", "iki", "uc", "necesen", "hello"]
+# print(my_list)
+# print(len(my_list))
+
+# Task - 9
+
+# helllllllpppppppppp
+
+# Task - 10 
+
+# my_list = ["qarpuz", "alca", "ciyelek", "banan", "papaya", "mango"]
+# print(my_list)
