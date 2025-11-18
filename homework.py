fce6cec53e986e24b72a342d9d4a58dec2700911
MehdiAgazade <agazademehdi371@gmail.com>
2025-11-18 20:17:50 +0400

    File created

---
+++ homework.py
@@ -0,0 +1,110 @@
+# Task - 11
+
+# my_list = [2, 3, 5, 4, 78, 8]
+
+# count = 0
+
+# for i in my_list:
+#     if i % 2 == 0:
+#         count += 1
+
+# print(count)
+
+# Task - 12
+
+# my_list = [2, 3, 5, 4, 78, 8]
+
+# for i in my_list:
+#     print(i * 2)
+
+# Task - 13
+
+# my_list = [2, 3, 5, 4, 78, 8]
+
+# big_num = my_list[0]
+
+# for i in my_list:
+#     if i > big_num:
+#         big_num = i
+
+# print(big_num)
+
+# Task - 14
+
+# my_list = [2, 3, 5, 4, 78, 8]
+
+# little_num = my_list[0]
+
+# for i in my_list:
+#     if i < little_num:
+#         little_num = i
+
+# print(little_num)
+
+# Task - 15
+
+# my_list = [2, 3, 5, 4, 78, 8]
+
+# my_list.reverse()
+# print(my_list)
+
+# Task - 16
+
+# my_list = [2, 3, 5, 4, 78, 8]
+
+# zero_list = []
+
+# for i in my_list:
+#     if i != 0:
+#         zero_list.append(i)
+
+# print(zero_list)
+
+# Task - 17
+
+# my_list = [2, 3, 5, 4, 78, 8]
+
+# for i in my_list:
+#     print(i ** 2)
+
+# Task - 18
+
+# my_list = [2, 3, 5, 4, 78, 8, 42, 43, 23, 33]
+
+# count = 0
+
+# for i in my_list:
+#     if i > 10:
+#         count += i
+
+# print(count)
+
+# Task - 19 
+
+# my_list = [2, 3, 5, 4, 78, 8, 42, 43, 23, 33]
+
+# for i in my_list:
+#     if i % 2 != 0:
+#         print(i)
+
+# Task - 20 
+
+# my_list = [2, 3, 5, 4, 78, 8, 42, 43, 23, 33]
+
+# for i in my_list:
+#     print(i == 7)
+
+# Task - 21
+
+# my_list = [2, 3, 5, 4, 78, 8, 42, 43, 23, 33]
+# my_list_2 = [1, 3, 4, 6, 8, 142, 645]
+
+# total_list = []
+
+# my_list.append(my_list_2)
+# total_list.append(my_list)
+
+# print(total_list)
+
+# Task - 22
+
--- main.py
+++ main.py
@@ -43,7 +43,6 @@
 
 # Task - 9
 
-# helllllllpppppppppp
 
 # Task - 10 
 
