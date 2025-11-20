19bf6aa1fe2dae0fe15490aa5c85daaa30b2b685
MehdiAgazade <agazademehdi371@gmail.com>
2025-11-20 23:11:01 +0400

    new task down

---
+++ tasks.py
@@ -0,0 +1,26 @@
+# def roman_nums(num):
+#     nums = [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 90, 80 ,70 ,60, 50, 40, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+#     symbols = ["M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"]
+
+#     roman_num = ""
+
+#     i = 0
+
+#     while num > 0:
+#         while num >= nums[i]:
+#             roman_num += symbols[i]
+#             num -= nums[i]
+#         i += 1
+#     return roman_num
+
+# user_date = input("Enter your birth date ( dd.mm.yyyy ) : ")
+
+# day, month, year = user_date.split(".")
+# day, month, year = int(day), int(month), int(year)
+
+
+# day = roman_nums(day)
+# month = roman_nums(month)
+# year = roman_nums(year)
+
+# print(f"{day}.{month}.{year}")
